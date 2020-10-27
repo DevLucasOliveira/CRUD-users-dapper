930237708e732218d4758cbcfed87c6a579dcaae
DevLucasOliveira <lucasoliveira.si@outlook.com>
2020-10-27 09:49:04 -0300

    Finalizando POC com dapper

---
+++ POC.Dapper/Controllers/CustomerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using POC.Dapper.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace POC.Dapper.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public CustomerController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            using (IDbConnection db = new SqlConnection(Configuration.GetConnectionString("CustomerConnection")))
+            {
+                customers = db.Query<Customer>("Select * From Customers").ToList();
+            }
+
+            return View(customers);
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
